d89d7faef62ad16800b17e34715946798b38eeb4
Eldor Togaymurotov <eldorcodes@gmail.com>
2020-12-19 19:06:54 -0500

    AUTH CONFIG REPLACED

--- src/base.js
+++ src/base.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAq8H_wtcxpkPpA6oGUCa_Ju5avgx6WTrw",
-    authDomain: "react-chatapp-1f70f.firebaseapp.com",
-    projectId: "react-chatapp-1f70f",
-    storageBucket: "react-chatapp-1f70f.appspot.com",
-    messagingSenderId: "1093784227778",
-    appId: "1:1093784227778:web:0f955f08d1b8bed122d05b",
-    measurementId: "G-V3L2VGGC96"
+    apiKey: "YOUR API KEY",
+    authDomain: "YOUR AUTH DOMAIN NAME",
+    projectId: "ID",
+    storageBucket: "BUCKET",
+    messagingSenderId: "MSG ID",
+    appId: "APP ID",
+    measurementId: "M ID"
   };
 
 // initialize firebase
